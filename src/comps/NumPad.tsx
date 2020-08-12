bea3a7edb1855f713364e9dcd837e559d348a148
Christian Cederfjärd <christian@cederfjard.com>
2020-08-12 23:01:37 +0100

    skeleton components

---
+++ src/comps/NumPad.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import NumPadButton from './NumPadButton'
+
+export default () => {
+  return (
+    <>
+    <NumPadButton />
+    <NumPadButton />
+    <NumPadButton />
+    <NumPadButton />
+    <NumPadButton />
+    <NumPadButton />
+    <NumPadButton />
+    <NumPadButton />
+    <NumPadButton />
+    <NumPadButton />
+    <NumPadButton />
+    <NumPadButton />
+    </>
+  );
+};
