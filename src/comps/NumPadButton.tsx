9ce439303c57246b5f7c5b112bcf2453ec555435
Christian Cederfjärd <christian@cederfjard.com>
2020-08-12 23:07:17 +0100

    styled button

--- src/comps/NumPadButton.tsx
+++ src/comps/NumPadButton.tsx
@@ -1,5 +1,30 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  width: 100px;
+  height: 100px;
+  font-size: 24px;
+  text-align: center;
+  cursor: pointer;
+  outline: none;
+  color: #fff;
+  background-color: #ccc;
+  border: none;
+  border-radius: 15px;
+  box-shadow: 0 9px #999;
+
+  &:hover {
+    background-color: #aaa;
+  }
+
+  &:active {
+    background-color: #aaa;
+    box-shadow: 0 5px #666;
+    transform: translateY(4px);
+  }
+`;
 
 export default () => {
-    return <button>Button</button>
-}
+  return <StyledButton>Button</StyledButton>;
+};
